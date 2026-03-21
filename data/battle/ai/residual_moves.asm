1305b940fab1bcf20b8c2d29ac3378a06faef132
crackpotfox <xcrackpotfoxx@gmail.com>
2026-03-21 09:45:07 -0400

    Implemented several AI improvements per Pret_s tutorial

--- data/battle/ai/residual_moves.asm
+++ data/battle/ai/residual_moves.asm
@@ -2,15 +2,19 @@
 
 ResidualMoves:
 	db MIST
-	db LEECH_SEED
 	db POISONPOWDER
 	db STUN_SPORE
 	db THUNDER_WAVE
 	db FOCUS_ENERGY
-	db BIDE
 	db POISON_GAS
 	db TRANSFORM
 	db CONVERSION
 	db SUBSTITUTE
 	db SPIKES
+	db TOXIC
+	db GLARE
+	db SPIDER_WEB
+	db BELLY_DRUM
+	db MEAN_LOOK
+	db ATTRACT
 	db -1 ; end
