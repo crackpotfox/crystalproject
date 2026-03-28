fb13958984d04b37394ff9809e917f9e30101c80
crackpotfox <xcrackpotfoxx@gmail.com>
2026-03-28 10:50:56 -0400

    Fixed FinalBattle from red

--- audio/music/redchamp.asm
+++ audio/music/redchamp.asm
@@ -1,13 +1,14 @@
+Music_FinalBattle:
 channel 1, Music_FinalBattle_Ch1
 channel 2, Music_FinalBattle_Ch2
 channel 3, Music_FinalBattle_Ch3
 
-Music_FinalBattle_Ch1::
+Music_FinalBattle_Ch1:
 	tempo 112
 	volume 7, 7
 	duty_cycle 3
 	vibrato 6, 3, 4
-	toggle_perfect_pitch
+;	toggle_perfect_pitch
 	note_type 12, 11, 2
 	octave 3
 	note F#, 1
@@ -383,7 +384,7 @@ Music_FinalBattle_Ch1::
 	note E_, 8
 	sound_loop 0, .mainloop
 
-Music_FinalBattle_Ch2::
+Music_FinalBattle_Ch2:
 	duty_cycle 3
 	vibrato 8, 2, 5
 	note_type 12, 12, 2
@@ -738,7 +739,7 @@ Music_FinalBattle_Ch2::
 	note D_, 2
 	sound_ret
 
-Music_FinalBattle_Ch3::
+Music_FinalBattle_Ch3:
 	note_type 12, 1, 1
 	octave 4
 	note E_, 4
